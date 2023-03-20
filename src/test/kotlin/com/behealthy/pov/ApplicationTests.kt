d4dd39e3ee86a6e5c1d98556070764672f3046e5
lowelllll <yejin2651@naver.com>
2023-03-21 01:38:25 +0900

    init project

---
+++ src/test/kotlin/com/behealthy/pov/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.behealthy.pov
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
